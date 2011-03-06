43c5174b081264bb31739eed6677bf641611c164
Stanley Semilla <semillastan@gmail.com>
2011-03-06 21:26:46 +0800

    uploaded all files

---
+++ urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls.defaults import *
+
+
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Example:
+    # (r'^ubus/', include('ubus.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    (r'^admin/', include(admin.site.urls)),
+)
